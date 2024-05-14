48122f47c380e2330ad50dff58d3a54646b02ffa
“TrieuNguyenTai” <taik36ntttq0902@gmail.com>
2024-05-15 02:39:57 +0700

    bth chương 4 slide 38 List ngày 8/5

--- baithuchanh/src/btchuong4/sl38.java
+++ baithuchanh/src/btchuong4/sl38.java
@@ -6,6 +6,7 @@ public class sl38 {
         LinkedList<String> linkedList = new LinkedList<String>();
         linkedList.add("java");
         linkedList.add("python");
+        linkedList.add("c++");
         System.out.println("Cac ngon ngu lap trinh: "+linkedList);
         System.out.println("So phan tu trong danh sach: "+linkedList.size());
         
