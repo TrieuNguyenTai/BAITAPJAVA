8fca3fe0f4af4e13c0b1ff3bb1461b0055467736
“TrieuNguyenTai” <taik36ntttq0902@gmail.com>
2024-05-15 00:41:47 +0700

    bth chương 2 slide 55 ngày 6/3

--- baithuchanh/src/btchuong2/baitaptrang55.java
+++ baithuchanh/src/btchuong2/baitaptrang55.java
@@ -2,7 +2,7 @@ package btchuong2;
 
 public class baitaptrang55 {
     public static void main(String[] args) {
-        System.out.println("cac so chia het cho 5: ");
+        System.out.println("cac so chia het cho 5 la: ");
     for (int i = 0; i <= 20; i++) {
        if (i % 5 != 0) {
               continue;
