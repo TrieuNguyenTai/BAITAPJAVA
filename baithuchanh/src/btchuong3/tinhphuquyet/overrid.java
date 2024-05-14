7628935e0a30504b5d936bd5a73c006998cbc6b0
“TrieuNguyenTai” <taik36ntttq0902@gmail.com>
2024-05-15 01:42:22 +0700

    bth chương 3 slide 66 ngày 24/4

--- baithuchanh/src/btchuong3/tinhphuquyet/overrid.java
+++ baithuchanh/src/btchuong3/tinhphuquyet/overrid.java
@@ -2,6 +2,6 @@ package btchuong3.tinhphuquyet;
 
 public class overrid {
     public void inttm(){
-        System.out.println("thư tính phủ");
+        System.out.println("thử tính phủ");
     }
 }
