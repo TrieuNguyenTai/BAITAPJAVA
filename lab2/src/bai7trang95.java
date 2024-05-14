3aea782f4ca0e63d80c35fd2ec7c74e4fddf2e83
“TrieuNguyenTai” <taik36ntttq0902@gmail.com>
2024-05-15 02:57:27 +0700

    bài 7 chương 2 slide 95 ngày 31/1

--- lab2/src/bai7trang95.java
+++ lab2/src/bai7trang95.java
@@ -1,6 +1,6 @@
 public class bai7trang95 {
     public static void main(String[] args) {
-        System.out.println("cac so nguyen duong tu 1->20 la:");
+        System.out.println("cac so nguyen duong tu 1->20:");
         for (int i = 1; i <= 20; i++) {
     System.out.println(i);
     
