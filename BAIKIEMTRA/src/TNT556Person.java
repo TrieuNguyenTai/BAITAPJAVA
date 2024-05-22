7a1b65251dee8e78a54c2b5cb9ed95c66bf5b3c8
“TrieuNguyenTai” <taik36ntttq0902@gmail.com>
2024-05-22 10:09:27 +0700

    TNT556_Cau2

--- BAIKIEMTRA/src/TNT556Person.java
+++ BAIKIEMTRA/src/TNT556Person.java
@@ -1,9 +1,29 @@
+import java.util.Scanner;
+
 public class TNT556Person {
 
     public String ten;
      public int tuoi;
     public int lop;
+    public void nhap(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap ten: ");
+        ten=sc.nextLine();
+
+        System.out.println("nhap tuoi: ");
+        tuoi=sc.nextInt ();
+        System.out.println("nhap lop: ");
+        lop= sc.nextInt();
+
+    }
+    public void xuat(){
+        System.out.println("ten:"+ ten);
+        System.out.println("tuoi: " +tuoi);
+        System.out.println("lop: ");
+    }
     public static void main(String[] args) {
-        
+    TNT556Person tn = new TNT556Person();
+    tn.nhap();
+    tn.xuat();
     }
 }
