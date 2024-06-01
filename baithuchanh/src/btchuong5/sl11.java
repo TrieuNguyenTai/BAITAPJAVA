11e1b60d6a1856c484181369b1e27fa3d4cee6de
“TrieuNguyenTai” <taik36ntttq0902@gmail.com>
2024-06-01 09:48:56 +0700

    bài tập chương5 sl11 ngày 29/5

---
+++ baithuchanh/src/btchuong5/sl11.java
@@ -0,0 +1,16 @@
+package btchuong5;
+
+public class sl11 {
+    public static void main(String[] args) {
+        try{
+            int tinhtoan = 37/6;
+        System.out.println(tinhtoan);
+        }catch(NullPointerException e){
+            System.out.println(e);
+        }finally{
+            System.out.println("khoi cuoi cung luon duoc thuc thi");
+        }
+        System.out.println("phan con lai cua doan code...");
+    }
+    
+}
